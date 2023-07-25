d0bfc101b7e18c32560740067a62344a1635011e
Yvan Janssens <yvanj@cyber.gent>
2023-07-25 13:24:52 +0200

    initial commit

---
+++ ibuild.py
@@ -0,0 +1,15 @@
+#!/QopenSys/pkgs/bin/python3
+
+# ibuild
+# Python-based IBM i build system.
+# Crufty, but does what I need it to do.
+# depends on: python3 python3-pyyaml
+# relies on a YAML file 'ibuild.yaml' located in $(pwd), example included 
+
+
+import yaml 
+
+
+
+def main():
+    #
